47145e50cff845cb3bf77fd8796b1db535a9b51c
hedgehog1029 <omgredstone@gmail.com>
2015-05-21 19:42:04 +0100

    Better responsiveness! More JavaScript for resizing.

--- project/js/main.js
+++ project/js/main.js
@@ -3,16 +3,21 @@ $(document).ready(function() {
     var offset1 = $(".menu").height() + $(".header").height();
     $("#bgvid").css("top", offset1);
     
-    $(".universe").css("height", $("#bgvid").height());
-    
     $(window).resize(function(e) {
         var offset = $(".menu").height() + $(".header").height();
         
         $("#bgvid").css("top", offset);
         $(".universe").css("height", $("#bgvid").height());
+        
+        if ($(window).innerHeight() < 1000 || $(window).innerWidth() < 1000) {
+            $(".sidebar").css("display", "none");
+        } else {
+            $(".sidebar").css("display", "block");
+        }
     });
         
     $(window).scroll(function(e) {
+        $(".universe").css("height", $("#bgvid").height());
         if ($(window).scrollTop() > 155) {
             $(".menu").addClass("fixed");
             $(".menu-anchor").addClass("expand");
@@ -20,5 +25,31 @@ $(document).ready(function() {
             $(".menu").removeClass("fixed");
             $(".menu-anchor").removeClass("expand");
         }
+        
+        if ($(window).scrollTop() < 863) {
+            removeAllActive();
+            $("#home").addClass("active");
+        } else if ($(window).scrollTop() > 863) {
+            removeAllActive();
+            $("#intranet").addClass("active");
+        }
     });
 });
+
+function removeAllActive() {
+    $(".menu ul").children("li").each(function(i) {
+        $(this).removeClass("active");
+    });
+}
+
+function goTo(s) {
+    if (s == "home") {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    } else if (s == "advert") {
+        $('html, body').animate({
+            scrollTop: 870
+        }, 1000);
+    }
+}
